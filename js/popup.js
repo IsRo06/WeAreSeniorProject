b6f0ef5623e13cd349e21753a6a02658da60d985
Summer Bossman <summer.bossman@gmail.com>
2026-02-06 12:07:37 -0500

    User can toggle between the dog and cat in the popup

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,13 @@
+const checkbox = document.getElementById('imageToggle');
+const imageElement = document.getElementById('myImage');
+
+checkbox.addEventListener("change", () => {
+	if(checkbox.checked){
+		imageElement.src = '/Images/Dog/DogStatic.png';
+	}
+	else{
+		imageElement.src = '/Images/Cat/CatStatic.png';
+	}
+});
+
 console.log("This is a popup!");
