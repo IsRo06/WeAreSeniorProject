5ea4d312e378592a0d9b4ec74070aba6aeda6cf0
Summer Bossman <summer.bossman@gmail.com>
2026-02-10 07:15:23 -0500

    Can now display text on page

--- js/content.js
+++ js/content.js
@@ -54,6 +54,41 @@ async function getPlannerItems() {
 
 }
 
+
+function renderSomething(element){
+	if(!element){
+		console.log("Alas");
+		return;
+	}
+	console.log("We're here");
+
+	const text = element.textContent;
+	const wordMatchRegExp = /[^\s]+/g; // Regular expression
+	const words = text.matchAll(wordMatchRegExp);
+	// matchAll returns an iterator, convert to array to get word count
+	const wordCount = [...words].length;
+	const readingTime = Math.round(wordCount / 200);
+	const badge = document.createElement("p");
+	console.log("Because we're here");
+	badge.textContent = `YO HO HO HO HO`;
+	/*
+	// Use the same styling as the publish information in an article's header
+	badge.classList.add("color-secondary-text", "type--caption");
+	badge.textContent = `⏱️ ${readingTime} min read`;
+
+	// Support for API reference docs
+	const heading = article.querySelector("h1");
+	// Support for article docs with date
+	const date = article.querySelector("time")?.parentNode;
+
+	(date ?? heading).insertAdjacentElement("afterend", badge);
+	*/
+	element.insertAdjacentElement("afterend", badge);
+}
+
+renderSomething(document.querySelector("aside"));
+
+
 /****** EXAMPLE TO PARSE TODO LIST ******/
 /*
 
