c4959655a8790c54ef01584aae335af0d954a4a9
Katheryn Abad <katherynabad@Katheryns-MacBook-Air.local>
2026-02-19 18:37:19 -0500

    Added Hungry Gifs. Tested on Popup. Changed Logo

--- js/popup.js
+++ js/popup.js
@@ -3,40 +3,58 @@ import { sum } from "/js/background.js";
 console.log("Yeehaw! 2 + 2 = " + sum(2, 2));
 
 const animalToggle = document.getElementById("animalToggle");
-const moodToggle = document.getElementById("moodToggle");
+const moodToggle = document.getElementById("moodToggle");     
+const hungerToggle = document.getElementById("hungerToggle");
 const imageElement = document.getElementById("myImage");
-
 
 const PATHS = {
   cat: {
     normal: "/Images/Cat/CatWagTail.gif",
     happy:  "/Images/Cat/CatHappy.gif",
+    hungry: "/Images/Cat/CatHungry.gif",
   },
   dog: {
     normal: "/Images/Dog/DogTailWag.gif",
-    happy:  "/Images/Dog/Happy Dog.gif", 
-  }
+    happy:  "/Images/Dog/Happy Dog.gif",
+    hungry: "/Images/Dog/DogHungry.gif",
+  },
 };
 
-chrome.storage.local.get(['dogSelected'], (data) => {
-	animalToggle.checked = !!data.dogSelected;
-	refreshImage();
-}); 
+chrome.storage.local.get(["dogSelected"], (data) => {
+  animalToggle.checked = !!data.dogSelected;
+  refreshImage();
+});
 
 function refreshImage() {
-	const animal = animalToggle.checked ? "dog" : "cat";
-	const mood = moodToggle.checked ? "happy" : "normal";
+  const animal = animalToggle.checked ? "dog" : "cat";
 
-	imageElement.src = PATHS[animal][mood];
+  const mood = hungerToggle.checked ? "hungry"
+             : moodToggle.checked   ? "happy"
+             : "normal";
+
+  imageElement.src = PATHS[animal][mood];
 }
 
-export function handleUserClick() {
-	refreshImage();
-
-	chrome.storage.local.set({dogSelected: animalToggle.checked});
+function handleAnimalToggle() {
+  chrome.storage.local.set({ dogSelected: animalToggle.checked });
+  refreshImage();
 }
 
+//mutually exclusive happy and hungry toggles
+function handleHappyToggle() {
+  if (moodToggle.checked) {
+    hungerToggle.checked = false;
+  }
+  refreshImage();
+}
 
-animalToggle.addEventListener("change", handleUserClick);
-moodToggle.addEventListener("change", handleUserClick);
+function handleHungryToggle() {
+  if (hungerToggle.checked) {
+    moodToggle.checked = false;
+  }
+  refreshImage();
+}
 
+animalToggle.addEventListener("change", handleAnimalToggle);
+moodToggle.addEventListener("change", handleHappyToggle);
+hungerToggle.addEventListener("change", handleHungryToggle);
