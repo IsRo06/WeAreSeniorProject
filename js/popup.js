15cb4ae60be320ecfcc3b973b89d11d4e11f0003
Isabella Roman <115611774+IsRo06@users.noreply.github.com>
2026-02-19 17:09:34 -0500

    Revert "Isabella ai implementation"

--- js/popup.js
+++ js/popup.js
@@ -1,42 +1,16 @@
-import { sum } from "/js/background.js";
+import { sum } from '/js/background.js'
 
 console.log("Yeehaw! 2 + 2 = " + sum(2, 2));
 
-const animalToggle = document.getElementById("animalToggle");
-const moodToggle = document.getElementById("moodToggle");
-const imageElement = document.getElementById("myImage");
+const checkbox = document.getElementById('imageToggle');
+const imageElement = document.getElementById('myImage');
 
-
-const PATHS = {
-  cat: {
-    normal: "/Images/Cat/CatWagTail.gif",
-    happy:  "/Images/Cat/CatHappy.gif",
-  },
-  dog: {
-    normal: "/Images/Dog/DogTailWag.gif",
-    happy:  "/Images/Dog/Happy Dog.gif", 
-  }
-};
-
-chrome.storage.local.get(['dogSelected'], (data) => {
-	animalToggle.checked = !!data.dogSelected;
-	refreshImage();
-}); 
-
-function refreshImage() {
-	const animal = animalToggle.checked ? "dog" : "cat";
-	const mood = moodToggle.checked ? "happy" : "normal";
-
-	imageElement.src = PATHS[animal][mood];
-}
-
-export function handleUserClick() {
-	refreshImage();
-
-	chrome.storage.local.set({dogSelected: animalToggle.checked});
-}
-
-
-animalToggle.addEventListener("change", handleUserClick);
-moodToggle.addEventListener("change", handleUserClick);
+checkbox.addEventListener("change", () => {
+	if(checkbox.checked){
+		imageElement.src = '/assets/images/Dog/DogWagTail.gif';
+	}
+	else{
+		imageElement.src = '/assets/images/Cat/CatWagTail.gif';
+	}
+});
 
