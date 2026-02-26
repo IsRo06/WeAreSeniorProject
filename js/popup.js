de2fbd0c97fb826c23dcc3b4fff87aa0ee5ff0ac
Isabella Roman <115611774+IsRo06@users.noreply.github.com>
2026-02-26 15:43:42 -0500

    Refactor image toggling logic and add mood handling

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,60 @@
-import { sum } from '/js/background.js'
+import { sum } from "/js/background.js";
 
 console.log("Yeehaw! 2 + 2 = " + sum(2, 2));
 
-const checkbox = document.getElementById('imageToggle');
-const imageElement = document.getElementById('myImage');
+const animalToggle = document.getElementById("animalToggle");
+const moodToggle = document.getElementById("moodToggle");     
+const hungerToggle = document.getElementById("hungerToggle");
+const imageElement = document.getElementById("myImage");
 
-checkbox.addEventListener("change", () => {
-	if(checkbox.checked){
-		imageElement.src = '/assets/images/Dog/DogWagTail.gif';
-	}
-	else{
-		imageElement.src = '/assets/images/Cat/CatWagTail.gif';
-	}
+const PATHS = {
+  cat: {
+    normal: "/Images/Cat/CatWagTail.gif",
+    happy:  "/Images/Cat/CatHappy.gif",
+    hungry: "/Images/Cat/CatHungry.gif",
+  },
+  dog: {
+    normal: "/Images/Dog/DogTailWag.gif",
+    happy:  "/Images/Dog/Happy Dog.gif",
+    hungry: "/Images/Dog/DogHungry.gif",
+  },
+};
+
+chrome.storage.local.get(["dogSelected"], (data) => {
+  animalToggle.checked = !!data.dogSelected;
+  refreshImage();
 });
 
+function refreshImage() {
+  const animal = animalToggle.checked ? "dog" : "cat";
+
+  const mood = hungerToggle.checked ? "hungry"
+             : moodToggle.checked   ? "happy"
+             : "normal";
+
+  imageElement.src = PATHS[animal][mood];
+}
+
+function handleAnimalToggle() {
+  chrome.storage.local.set({ dogSelected: animalToggle.checked });
+  refreshImage();
+}
+
+//mutually exclusive happy and hungry toggles
+function handleHappyToggle() {
+  if (moodToggle.checked) {
+    hungerToggle.checked = false;
+  }
+  refreshImage();
+}
+
+function handleHungryToggle() {
+  if (hungerToggle.checked) {
+    moodToggle.checked = false;
+  }
+  refreshImage();
+}
+
+animalToggle.addEventListener("change", handleAnimalToggle);
+moodToggle.addEventListener("change", handleHappyToggle);
+hungerToggle.addEventListener("change", handleHungryToggle);
